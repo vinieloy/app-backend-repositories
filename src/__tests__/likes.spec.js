050c0a46e4c8556f941d66712718ff27bdd10c06
Vinicius Eloy <paulo.v.almeida@globalhitss.com.br>
2020-08-19 20:32:41 -0300

    refactor(tests): refactor api tests

--- src/__tests__/likes.spec.js
+++ src/__tests__/likes.spec.js
@@ -1,14 +1,15 @@
 const request = require("supertest");
 const app = require("../app");
 
+
 describe("Likes", () => {
   it("should be able to give a like to the repository", async () => {
     const repository = await request(app)
       .post("/repositories")
       .send({
-        url: "https://github.com/Rocketseat/umbriel",
-        title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        url: "https://github.com/vinieloy/app-redis-repositories",
+        title: "Redis",
+        techs: ["Docker", "Javascript", "Redis"]
       });
 
     let response = await request(app).post(
@@ -28,6 +29,7 @@ describe("Likes", () => {
     });
   });
 
+  
   it("should not be able to like a repository that does not exist", async () => {
     await request(app)
       .post(`/repositories/123/like`)
--- src/__tests__/repositories.spec.js
+++ src/__tests__/repositories.spec.js
@@ -124,29 +124,4 @@ describe("Repositories", () => {
   it("should not be able to delete a repository that does not exist", async () => {
     await request(app).delete(`/repositories/123`).expect(400);
   });
-
-
-  it("should be able to give a like to the repository", async () => {
-    const repository = await request(app)
-      .post("/repositories")
-      .send({
-        url: "https://github.com/vinieloy/app-redis-repositories",
-        title: "Redis",
-        techs: ["Docker", "Javascript", "Redis"]
-      });
-    
-    expect(isUuid(repository.body.id)).toBe(true);
-
-    const response = await request(app)
-      .post(`/repositories/${repository.body.id}/like`);
-
-    expect(response.body).toMatchObject({
-      likes: 1
-    });
-  });
-
-
-  it("should not be able to like a repository that does not exist", async () => {
-    await request(app).post(`/repositories/123/like`).expect(400);
-  });
 });
