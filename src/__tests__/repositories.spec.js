c5760cc5a45079e59f98a75636981895168f5421
Vinicius Eloy <paulo.v.almeida@globalhitss.com.br>
2020-08-19 20:28:25 -0300

    refactor(tests): refactor and add api tests

--- src/__tests__/repositories.spec.js
+++ src/__tests__/repositories.spec.js
@@ -2,33 +2,35 @@ const request = require("supertest");
 const app = require("../app");
 const { isUuid } = require("uuidv4");
 
+
 describe("Repositories", () => {
   it("should be able to create a new repository", async () => {
     const response = await request(app)
       .post("/repositories")
       .send({
-        url: "https://github.com/Rocketseat/umbriel",
-        title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        url: "https://github.com/vinieloy/app-redis-repositories",
+        title: "Redis",
+        techs: ["Docker", "Javascript", "Redis"]
       });
 
     expect(isUuid(response.body.id)).toBe(true);
 
     expect(response.body).toMatchObject({
-      url: "https://github.com/Rocketseat/umbriel",
-      title: "Umbriel",
-      techs: ["Node", "Express", "TypeScript"],
+      url: "https://github.com/vinieloy/app-redis-repositories",
+      title: "Redis",
+      techs: ["Docker", "Javascript", "Redis"],
       likes: 0
     });
   });
+
 
   it("should be able to list the repositories", async () => {
     const repository = await request(app)
       .post("/repositories")
       .send({
-        url: "https://github.com/Rocketseat/umbriel",
-        title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        url: "https://github.com/vinieloy/app-redis-repositories",
+        title: "Redis",
+        techs: ["Docker", "Javascript", "Redis"]
       });
 
     const response = await request(app).get("/repositories");
@@ -37,28 +39,29 @@ describe("Repositories", () => {
       expect.arrayContaining([
         {
           id: repository.body.id,
-          url: "https://github.com/Rocketseat/umbriel",
-          title: "Umbriel",
-          techs: ["Node", "Express", "TypeScript"],
+          url: "https://github.com/vinieloy/app-redis-repositories",
+          title: "Redis",
+          techs: ["Docker", "Javascript", "Redis"],
           likes: 0
         }
       ])
     );
   });
 
+
   it("should be able to update repository", async () => {
     const repository = await request(app)
       .post("/repositories")
       .send({
-        url: "https://github.com/Rocketseat/umbriel",
-        title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        url: "https://github.com/vinieloy/app-redis-repositories",
+        title: "Redis",
+        techs: ["Docker", "Javascript", "Redis"]
       });
 
     const response = await request(app)
       .put(`/repositories/${repository.body.id}`)
       .send({
-        url: "https://github.com/Rocketseat/unform",
+        url: "https://github.com/vinieloy/unform",
         title: "Unform",
         techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
       });
@@ -66,23 +69,25 @@ describe("Repositories", () => {
     expect(isUuid(response.body.id)).toBe(true);
 
     expect(response.body).toMatchObject({
-      url: "https://github.com/Rocketseat/unform",
+      url: "https://github.com/vinieloy/unform",
       title: "Unform",
       techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
     });
   });
 
+
   it("should not be able to update a repository that does not exist", async () => {
     await request(app).put(`/repositories/123`).expect(400);
   });
+
 
   it("should not be able to update repository likes manually", async () => {
     const repository = await request(app)
       .post("/repositories")
       .send({
-        url: "https://github.com/Rocketseat/umbriel",
-        title: "Umbriel",
-        techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
+        url: "https://github.com/vinieloy/app-redis-repositories",
+        title: "Redis",
+        techs: ["Docker", "Javascript", "Redis"]
       });
 
     const response = await request(app)
@@ -96,11 +101,12 @@ describe("Repositories", () => {
     });
   });
 
+
   it("should be able to delete the repository", async () => {
     const response = await request(app)
       .post("/repositories")
       .send({
-        url: "https://github.com/Rocketseat/umbriel",
+        url: "https://github.com/vinieloy/umbriel",
         title: "Umbriel",
         techs: ["Node", "Express", "TypeScript"]
       });
@@ -114,7 +120,33 @@ describe("Repositories", () => {
     expect(repository).toBe(undefined);
   });
 
+
   it("should not be able to delete a repository that does not exist", async () => {
     await request(app).delete(`/repositories/123`).expect(400);
+  });
+
+
+  it("should be able to give a like to the repository", async () => {
+    const repository = await request(app)
+      .post("/repositories")
+      .send({
+        url: "https://github.com/vinieloy/app-redis-repositories",
+        title: "Redis",
+        techs: ["Docker", "Javascript", "Redis"]
+      });
+    
+    expect(isUuid(repository.body.id)).toBe(true);
+
+    const response = await request(app)
+      .post(`/repositories/${repository.body.id}/like`);
+
+    expect(response.body).toMatchObject({
+      likes: 1
+    });
+  });
+
+
+  it("should not be able to like a repository that does not exist", async () => {
+    await request(app).post(`/repositories/123/like`).expect(400);
   });
 });
